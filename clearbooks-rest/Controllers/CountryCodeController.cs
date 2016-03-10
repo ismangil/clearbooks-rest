349d968a1dc1765d0be901898f6212e12f3725ae
Perry Ismangil <perry@ismangil.com>
2016-03-10 09:41:24 +0000

    Added country code translator

---
+++ clearbooks-rest/Controllers/CountryCodeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace clearbooks_rest.Controllers
+{
+    public class CountryCodeController : ApiController
+    {
+
+        // GET: api/CountryCode/5
+        /// <summary>
+        /// Given the country name returns two letter code
+        /// </summary>
+        /// <param name="countryName"></param>
+        /// <returns>Two letter country code</returns>
+        public string Get(string countryName)
+        {
+            var countries = new Bia.Countries.Iso3166Countries(StringComparer.OrdinalIgnoreCase);
+
+            return countries.GetAlpha2CodeByName(countryName);
+        }
+    }
+}
